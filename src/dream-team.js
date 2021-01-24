6344f0c5b14f52f6fa993e855469b13505b31060
sergJess <serg.fantasy@yandex.ru>
2021-01-24 10:46:34 +0300

    feat: add solution for task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if ((Array.isArray(members) === true) && members.length >= 1) {
+        let firstLetter = 0;
+        let stringFirstLetters = '';
+
+        function filterFunc(value) {
+            if (typeof(value) === 'string')
+                return value.trim();
+        }
+        let stringArray = members.filter(filterFunc);
+        let length = stringArray.length;
+        for (let i = 0; i < length; i++) {
+            let strWithoutSpaces = stringArray[i].trim();
+            stringFirstLetters += strWithoutSpaces[firstLetter].toUpperCase();
+        }
+
+        return stringFirstLetters.split('').sort().join('');
+    }
+
+
+    return false;
 };
